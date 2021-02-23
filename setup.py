ead4f5525a51f99ce341c0b1ace07baf9ebc8798
DannyJacoby <60196483+DannyJacoby@users.noreply.github.com>
2021-02-23 12:45:59 -0800

    Initial Commit w/helloWorld

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cst438_sp21_p2',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='danth',
+    author_email='djacoby@csumb.edu',
+    description='Project 2 '
+)
