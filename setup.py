27b6587d1708d20a0190bc9a4956646ab1baa478
DannyJacoby <60196483+DannyJacoby@users.noreply.github.com>
2021-02-25 12:13:15 -0800

    Pushing Python envo and python scripts

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cst438_sp21_p2',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='Danny',
+    author_email='djacoby@csumb.edu',
+    description='Project 2'
+)
