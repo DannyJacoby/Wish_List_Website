358210d90ab150eef21146d08a50d9542ba0e614
DannyJacoby <60196483+DannyJacoby@users.noreply.github.com>
2021-03-04 13:03:12 -0800

    Possibly ready for heroku

--- api/api.py
+++ api/api.py
@@ -1,9 +1,8 @@
-import time
 from flask import Flask, send_from_directory
 from flask_cors import CORS, cross_origin
+import time
 
 app = Flask(__name__, static_folder='frontend/build', static_url_path='')
-
 
 @app.route("/time")
 @cross_origin()
@@ -13,13 +12,12 @@ def get_current_time():
 @app.route('/')
 @cross_origin()
 def index():
-    return "Welcome to our Server!!"
+    return "<h1>Welcome to our Server!!</h1>"
 
 @app.route('/index')
 @cross_origin()
 def homepage():
     return send_from_directory(app.static_folder,'index.html')
     
-
 if __name__ == '__main__':
     app.run()
