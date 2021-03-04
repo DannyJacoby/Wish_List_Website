49f8a696c07fa2a790023b0b12416b7a0b76f653
DannyJacoby <60196483+DannyJacoby@users.noreply.github.com>
2021-03-04 12:56:17 -0800

    Updated basic stuff

--- api/api.py
+++ api/api.py
@@ -1,16 +1,25 @@
 import time
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend/build', static_url_path='')
 
 
 @app.route("/time")
+@cross_origin()
 def get_current_time():
     return {"time": time.time()}
 
 @app.route('/')
+@cross_origin()
 def index():
     return "Welcome to our Server!!"
 
-if __name__ == "__main__":
+@app.route('/index')
+@cross_origin()
+def homepage():
+    return send_from_directory(app.static_folder,'index.html')
+    
+
+if __name__ == '__main__':
     app.run()
