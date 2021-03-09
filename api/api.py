e5484ce23db2474ffb2f17790775596aeaada5d3
DannyJacoby <60196483+DannyJacoby@users.noreply.github.com>
2021-03-09 12:25:58 -0800

    Possibly going to work this time maybe
    https://medium.com/swlh/how-to-deploy-a-react-python-flask-project-on-heroku-edb99309311
    following this tutorial to deploy to heroku

--- api/api.py
+++ api/api.py
@@ -2,7 +2,7 @@ from flask import Flask, send_from_directory
 from flask_cors import CORS, cross_origin
 import time
 
-app = Flask(__name__, static_folder='frontend/', static_url_path='')
+app = Flask(__name__, static_folder='./build', static_url_path='/')
 
 @app.route("/time")
 @cross_origin()
@@ -18,6 +18,10 @@ def index():
 @cross_origin()
 def homepage():
     return send_from_directory(app.static_folder,'index.html')
-    
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT',80))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var herokuURL="https://wishlist-cst438.herokuapp.com/time"
+var localHost="http://localhost:5000/time"
+
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(() => {
-    fetch("http://localhost:5000/time").then(res => res.json()).then(data => {
+    fetch(herokuURL).then(res => res.json()).then(data => {
       setCurrentTime(data.time);
     });
   }, []);
