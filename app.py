72eee1736612dbe103363cd58b33c412011be63e
DannyJacoby <60196483+DannyJacoby@users.noreply.github.com>
2021-03-11 12:23:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory
 from flask_cors import CORS, cross_origin
-from pymysql import MySQL
+# from flask_mysqldb import MySQL
 import time
 
 # As a note, all references to flask's app, hereby called api, is the app name in Procfile, 
