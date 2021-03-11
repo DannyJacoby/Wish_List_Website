96d1f355cc28110286ff4687ad5f3cb743bcc4f8
Tessa <trosasco@csumb.edu>
2021-03-11 09:53:31 -0800

    added react routing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,13 +2,13 @@ import Button from './Button'
 
 const Header = ({ title }) => {
     const onClick = () => {
-        console.log('Click')
+        console.log('Clicked')
     }
 
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='lightblue' text='Add' onClick=
+            <Button color='lightblue' text='Login' onClick=
             {onClick}/>
         </header>
     )
