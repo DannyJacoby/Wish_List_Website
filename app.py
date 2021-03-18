cee66d1f950152bec75a8f3a9acd83fe8186512a
Garrett Slone <gsloneca@gmail.com>
2021-03-17 22:24:18 -0700

    setup session create and destroy

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import time
 
-from flask import Flask, request, redirect, url_for, session, send_from_directory
+from flask import Flask, request, redirect, Response, url_for, session
 from flask_cors import CORS
 from flask_session import Session
 
-from middleware import logged_in, is_admin
-
+from middleware import logged_in, admin
 
 # As a note, all references to flask's app, hereby called api, is the app name in Procfile,
 # while this file's name is the name bit of Procfile
 # basically, if change "api", change Procfile's (web: gunicorn app:api) api's to new thing
 api = Flask(__name__, static_folder="frontend/build", static_url_path="")
+api.secret_key = "eTmic_1_EPw8UTpxt7xMJQ"
+api.config["SESSION_TYPE"] = "filesystem"
 CORS(api)
 Session(api)
 
@@ -22,15 +23,26 @@ Session(api)
 # api.config['MYSQL_DB'] = 'n5msszwbmtsqj22r'
 # mysql = MySQL(api)
 
+def _session_create(username, is_admin):
+    session["user"] = username
+    session["is_admin"] = is_admin
+
+
+def _session_destroy():
+    session.pop("user", None)
+    session.pop("is_admin", None)
+
 
 @api.route("/time")
 def get_current_time():
     return {"time": time.time()}
 
+
 @api.route("/")
 @api.route("/index")
 def index():
     return api.send_static_file("index.html")
+
 
 @api.route("/login", methods=["GET", "POST"])
 def login():
@@ -39,9 +51,13 @@ def login():
 
     # POST
     else:
-        return redirect(url_for("profile"))
-    #   username = request.form.get("user")
-    #   password = request.form.get("pass")
+        username = request.form.get("user")
+        password = request.form.get("pass")
+
+        _session_create(username, False)
+
+        return redirect(url_for("profile")) if username is not None else Response("Username/Password not found",
+                                                                                  mimetype="text/plain", status=401)
     # TODO: check if the user entered valid credentials
     #      - on success: set up their session and redirect to the landing page
     #      - on failure: render an error message
@@ -49,7 +65,7 @@ def login():
 
 @api.route('/logout')
 def logout():
-    session.pop('username', None)
+    _session_destroy()
     return redirect(url_for('index'))
 
 
@@ -60,9 +76,13 @@ def create_account():
 
     # POST
     else:
-        return redirect(url_for("profile"))
-    #     username = request.form.get("user")
-    #     password = request.form.get("pass")
+        username = request.form.get("user")
+        password = request.form.get("pass")
+
+    _session_create(username, False)
+
+    return redirect(url_for("profile")) if username is not None else Response("Failed to create account",
+                                                                              mimetype="text/plain", status=500)
     # TODO: check if the user entered valid credentials
     #      - on success: add them to the db and redirect to the landing page
     #      - on failure: render an error message
@@ -77,15 +97,17 @@ def profile():
 
 @api.route("/admin")
 @logged_in
-@is_admin
+@admin
 def admin():
     # TODO: add middleware function to make sure that a user must be logged
     #       in to see this page, and that they can only see their page
     return api.send_static_file("admin.html")
-  
+
+
 @api.errorhandler(404)
 def not_found(e):
     return api.send_static_file("index.html")
+
 
 if __name__ == '__main__':
     api.run()
