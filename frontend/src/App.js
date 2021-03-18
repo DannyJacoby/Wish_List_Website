b6b6342adc0f7c652b8eaefc4aa1f0e329b24d43
Tessa <trosasco@csumb.edu>
2021-03-18 11:02:07 -0700

    added nav bar and links working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import Header from './components/Header'
+import Header from './components/Header';
 import './App.css';
 import Button from './components/Button';
-import { NavLink, Switch, Route, Router } from 'react-router-dom';
+import Nav from './components/Nav';
+import Profile from './components/Profile';
+import EditProfile from './components/EditProfile';
+import Login from './components/Login'
+import Home from './components/Home'
+import CreateAcc from './components/CreateAcc'
+import { NavLink, Switch, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 var herokuURL="https://wishlist-cst438.herokuapp.com/time"
@@ -19,69 +25,21 @@ function App() {
 
   return (
     <BrowserRouter>
-    <div className="App"> 
-      <h1>      
-        <Header title={"Wishlist App"}/>
-      </h1>
-      <Navigation />
-      <Main />
-      <div className="btn">
-        <Button text={"Create Account"}/>
+      <div className="App"> 
+        <Nav />
+          <h1>      
+            <Header title={"Wishlist App"}/>
+          </h1>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/login" component={Login}></Route>
+          <Route exact path="/create_account" component={CreateAcc}></Route>
+          <Route exact path="/profile" component={Profile}></Route>
+          <Route exact path="/edit_profile" component={EditProfile}></Route>
+        </Switch>
       </div>
-
-    </div>
     </BrowserRouter>
   );
 }
-
-
-const Navigation = () => (
-  <nav>
-    <ul>
-      <li><NavLink to='/'>Home</NavLink></li>
-      <li><NavLink to='/login'>Login</NavLink></li>
-      <li><NavLink to='/createaccount'>CreateAccount</NavLink></li>
-      <li><NavLink to='/profile'>Profile</NavLink></li>
-    </ul>
-  </nav>
-);
-
-const Home = () => (
-  <div className='home'>
-    <h1>Welcome to our wishlist app!</h1>
-    <p> Create an account or login to get started.</p>
-  </div>
-);
-
-const Login = () => (
-  <div className='login'>
-    <h1>Login Page</h1>
-    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-    
-  </div>
-);
-
-const CreateAcc = () => (
-  <div className='createaccount'>
-    <h1>Create an Account!</h1>
-    <p>Enter your name, username, email, and password to get started.<strong>hello@example.com</strong></p>
-  </div>
-);
-
-const Profile = () => (
-  <div className='profile'>
-    <h1>Profile Page</h1>
-    <p> Create or view your wishlists here.</p>
-  </div>
-);
-
-const Main = () => (
-  <Switch>
-    <Route path='/' component={Home}></Route>
-    <Route path='/login' component={Login}></Route>
-    <Route path='/createaccount' component={CreateAcc}></Route>
-    <Route path='/profile' component={Profile}></Route>
-  </Switch>
-);
 
 export default App;
---
+++ frontend/src/components/EditProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function EditProfile() {
+    return (
+        <div>
+            <h1>Edit Profile Page</h1>
+        </div>
+    )
+}
+
+export default EditProfile;
