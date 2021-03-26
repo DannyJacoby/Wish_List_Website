e4d027a04e123ddcb1aee5ccb32b614764f43af9
Garrett Slone <gsloneca@gmail.com>
2021-03-25 22:49:46 -0700

    fix make_req to take data for PUTs and fix nav menu

--- frontend/static/js/req.js
+++ frontend/static/js/req.js
@@ -1,20 +1,27 @@
 /* Make a request to the API (since PUT and DELETE can't
  * sent in a form).
- * id - the id of the resource for the request (user_id or item_id)
  * route - route to send the request to
  * method - HTTP method to use (PUT or DELETE)
  * target_id - id of the html element to target with the response
  *             message
+ * data - the json data to pass to PUT requests
  */
-let make_req = async (id, route, method, target_id) => {
-    route += id ? id.toString() : "";
+let make_req = async (route, method, target_id, data) => {
+    let init =  {method: method};
 
-    let json = await fetch(route, {method: method}).then(response => response.json());
-
-    if (json.redirect) {
-        window.location.href = json.redirect;
+    if (method === "PUT") {
+        init.headers = {'Content-Type': 'application/json'};
+        init.body = JSON.stringify(data);
     }
+
+    let json = await fetch(route, init)
+        .then(response => response.json())
+        .catch(e => alert(e));
 
     let target = document.getElementById(target_id);
     target.innerText = json["message"];
+
+    setTimeout(() => {
+        json.redirect ? window.location.href = json.redirect : window.location.reload();
+    }, 1500);
 }
